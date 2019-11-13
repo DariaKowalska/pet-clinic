3b42d2d24be6e11f379edecd2122ef830f817ce8
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-11-13 09:04:20 +0100

    abstract Id generation

--- src/main/java/com/myproject/petclinic/bootstrap/DataLoader.java
+++ src/main/java/com/myproject/petclinic/bootstrap/DataLoader.java
@@ -21,14 +21,14 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+
         owner1.setFirstName("Fiona");
         owner1.setLastName("Glenanne");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+
         owner2.setFirstName("Michael");
         owner2.setLastName("Wiston");
 
@@ -37,14 +37,14 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+
         vet1.setFirstName("Sara");
         vet1.setLastName("Kim");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
+
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
 

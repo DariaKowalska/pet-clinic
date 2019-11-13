143e2ac296904699bc1bd6c6e8071dbe1ac20a0f
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-11-13 07:59:08 +0100

    implements interfaces for services

---
+++ src/main/java/com/myproject/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.myproject.petclinic.services;
+
+import com.myproject.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ src/main/java/com/myproject/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.myproject.petclinic.services;
+
+import com.myproject.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}

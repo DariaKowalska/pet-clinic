ffbde9c17dd10065984674c4031ca55a69a9da4b
Daria Peziak-Kowalska <d.peziak.kowalska@gmail.com>
2019-11-18 12:16:28 +0100

    Added Specialites Service and MapService

---
+++ src/main/java/com/myproject/petclinic/services/SpecialitesService.java
@@ -0,0 +1,6 @@
+package com.myproject.petclinic.services;
+
+import com.myproject.petclinic.model.Speciality;
+
+public interface SpecialitesService extends CrudService<Speciality, Long> {
+}
---
+++ src/main/java/com/myproject/petclinic/services/map/SpecialitesMapService.java
@@ -0,0 +1,37 @@
+package com.myproject.petclinic.services.map;
+
+import com.myproject.petclinic.model.Speciality;
+import com.myproject.petclinic.services.SpecialitesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialitesMapService extends AbstractMapService<Speciality, Long> implements SpecialitesService {
+    @Override
+    public Set<Speciality> findAll() {
+
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+
+        return super.findById(id);
+    }
+}
